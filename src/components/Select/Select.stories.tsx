f469c32dc311a1e138f380d3d17b29b733c62831
Kkatsiarryna <katepicastor@gmail.com>
2024-08-03 19:19:38 +0300

    select component

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,19 +1,43 @@
 
 import {Select} from "./Select";
-import React from "react";
+import React, {useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export default {
     title: 'Select',
     component: Select
 }
 
-export const FirstSelect = () => {
-    return <Select value={'someValue'}
-                   onChange={() => {}}
-                   items={[
-                    {title: 'Katya', value: 1 },
-                    {title: 'Sofa' , value: 2 },
-                    {title: 'Lena' , value: 3 },
-                   ]}
-                   />
+export const WithValue = () => {
+    const [value, setvalue] = useState(null);
+    return (
+        <>
+            <Select
+                onChange={setvalue}
+                value={value}
+                items={[
+                    { value: 1, title: 'Katya'},
+                    { value: 2, title: 'Sofa' },
+                    { value: 3, title: 'Lena'},
+                ]}
+            />
+        </>
+        )
+}
+
+
+
+export const WithoutValue = () => {
+    const [value, setvalue] = useState(null);
+    return <>
+        <Select
+            value={value}
+                onChange={setvalue}
+                items={[
+                    {title: 'Katya', value: 4 },
+                    {title: 'Sofa' , value: 5},
+                    {title: 'Lena' , value: 6 },
+                ]}
+        />
+    </>
 }
